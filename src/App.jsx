b1db85e2287270e46778eb5b041f8ee6c6f356d0
AilinNavas <navasailin18@gmail.com>
2024-01-11 19:02:34 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@
 import './App.css'
 import DentalInsurance from './components/DentalInsurance'
 import Hero from './components/Hero'
-import Prueba from './components/Prueba'
+
+
+
 
 function App() {
 
 
+
   return (
     <>
- 
-    <Hero/>
-   <DentalInsurance/> 
+  
+      
+
+     <Hero />
+      <DentalInsurance />
+
+     
+
     </>
   )
 }
--- src/components/DentalInsurance.jsx
+++ src/components/DentalInsurance.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import InsuranceList from './InsuranceList'
-import { motion as m } from 'framer-motion'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 
 const DentalInsurance = () => {
 
     return (
-        <m.div className='bg-primary h-screen'>
+      
+        <div className='bg-primary h-screen'>
           <div className='max-w-[1200px] text-center m-auto'>
             <h2 className='text-4xl text-white text-center py-5 md:text-5xl lg:text-6xl lg:mb-10 pt-14'>Dental Insurance</h2>
             <p className='text-xl text-white text-center mb-4 px-8 md:text-2xl lg:text-4xl lg:px-20 '>We accept most dental insurance, including out-of-network, so don't hesitate to schedule with us.</p>
@@ -15,7 +17,10 @@ const DentalInsurance = () => {
             <InsuranceList />
             <p className='pt-16 text-xl text-white text-center mb-10 px-8 md:text-2xl lg:text-4xl lg:px-20 lg:pt-20'>We will do everything possible to maximize your benefits and we can give you a breakdown of those benefits before we do treatment.</p>
           </div>
-        </m.div>
+
+        
+        </div>
+       
       );
     };
 
