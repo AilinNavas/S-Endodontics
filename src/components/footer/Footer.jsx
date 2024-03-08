ba46c76cd33274d68907f8dd7b52b316ae447a0b
AilinNavas <navasailin18@gmai.com>
2024-03-08 02:29:39 -0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
 
 
 return (
-<footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer className="bg-white shadow mt-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href='' className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
