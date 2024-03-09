b104584967fc9f7ba3a22270d091b9238f1b1048
AilinNavas <navasailin18@gmai.com>
2024-03-09 08:13:38 -0300

    cambios animacion bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
+import React from 'react'
 import './App.css'
 import { WrapperContainer } from './components/welcomeSection/WrapperContainer'
 import ActiveSlider from './components/testimonialsSection/ActiveSlider'
 import { AccordionContainer } from './components/faqsSection/AccordionContainer'
 import Footer from './components/footerSection/Footer'
-import { gsap } from 'gsap/all'
-import AnimationContext from './context/animation-context';
 import AboutUs from './components/aboutUsSection/AboutUs'
 import DentalInsurance from './components/insuranceSection/DentalInsurance';
 import { Pricing } from './components/pricing/Pricing';
 
 function App() {
-  const [currentBg, setCurrentBg] = useState('#fff')
-  let appRef = useRef(null);
-
-  useEffect(() => {
-    gsap.to(appRef.current, {
-      duration: 1,
-      background: currentBg,
-    })
-  }, [currentBg]);
 
   return (
+    <>
+      <WrapperContainer />
+      <DentalInsurance />
+      <Pricing />
+      <ActiveSlider />
+      <AboutUs />
+      <AccordionContainer />
+      <Footer />
+    </>
 
-
-    <AnimationContext.Provider value={{ setCurrentBg }}>
-      <div ref={appRef} className="App">
-
-        <WrapperContainer />
-        <DentalInsurance />
-        <Pricing />
-        <ActiveSlider />
-        <AboutUs/>
-        <AccordionContainer />
-        <Footer />
-
-      </div>
-    </AnimationContext.Provider>
   )
 
 
