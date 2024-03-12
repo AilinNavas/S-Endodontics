5b53f3799106a2ef8f6d53111c21268f27aa18b0
AilinNavas <navasailin18@gmai.com>
2024-03-12 06:08:41 -0300

    coment

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './App.css'
 import { WrapperContainer } from './components/WelcomeSection/WrapperContainer';
 import { DentalInsurance } from './components/InsuranceSection/DentalInsurance';
-import { Pricing } from './components/PricingSection/Pricing';
-import { ActiveSlider } from './components/TestimonialsSection/ActiveSlider';
-import { AboutUs } from './components/AboutUsSection/AboutUs';
-import { AccordionContainer } from './components/FaqsSection/AccordionContainer';
+// import { Pricing } from './components/PricingSection/Pricing';
+// import { ActiveSlider } from './components/TestimonialsSection/ActiveSlider';
+// import { AboutUs } from './components/AboutUsSection/AboutUs';
+// import { AccordionContainer } from './components/FaqsSection/AccordionContainer';
 // import { Footer } from './components/FooterSection/Footer';
 
 
@@ -17,10 +17,10 @@ function App() {
     <>
       <WrapperContainer />
       <DentalInsurance />
-      <Pricing />
+      {/* <Pricing />
       <ActiveSlider />
       <AboutUs />
-      <AccordionContainer />
+      <AccordionContainer /> */}
       {/* <Footer /> */}
     </>
 
